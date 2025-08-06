c9d0b4a6e603e83bf11b35e78ab06edb87e7d1f0
Junsu An <90178502+junsu-a@users.noreply.github.com>
2025-08-05 17:03:14 -0700

    Apply gruvbox theme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" }
+
+  -- Configure LazyVim ti load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  }
+}
