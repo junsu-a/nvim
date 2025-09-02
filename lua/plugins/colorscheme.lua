288df5e61af7e76745d9ea84ea99ad49685d219a
Junsu An <90178502+junsu-a@users.noreply.github.com>
2025-09-02 13:35:38 -0700

    colorscheme.lua 업데이트

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,12 @@
 return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
-
+  { 
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    opts ={},
+  },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
