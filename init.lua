cbe877304ef4663bbf3791da0c2365cd7d078eec
Junsu An <90178502+junsu-a@users.noreply.github.com>
2025-09-03 16:48:16 -0700

    colorcolumn highlight

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "80,100"
+vim.cmd [[highlight ColorColumn cterbg=lightgrey guibg=lightgrey]]
