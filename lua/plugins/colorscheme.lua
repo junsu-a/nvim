c97a9eb9f13e677d7220cc7f486a6fce092faa88
Junsu An <90178502+junsu-a@users.noreply.github.com>
2025-10-09 10:44:26 -0700

    Comment all colorscheme.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,17 @@
-return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-  { 
-    "craftzdog/solarized-osaka.nvim",
-    lazy = false,
-    priority = 1000,
-    opts ={},
-  },
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "wildcharm",
-    },
-  },
-}
+-- return {
+--   -- add gruvbox
+--   { "ellisonleao/gruvbox.nvim" },
+--   { 
+--     "craftzdog/solarized-osaka.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     opts ={},
+--   },
+--   -- Configure LazyVim to load gruvbox
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "wildcharm",
+--     },
+--   },
+-- }
